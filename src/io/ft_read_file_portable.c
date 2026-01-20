de81f097b46357dd540cec06753eed2ef9958805
j <castro.jaime@proton.me>
2026-01-20 05:13:24 +0100

    fix: leak in ft_read_file_portable.c

--- src/io/ft_read_file_portable.c
+++ src/io/ft_read_file_portable.c
@@ -6,7 +6,7 @@
 /*   By: jaicastr <jaicastr@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/20 03:52:23 by jaicastr          #+#    #+#             */
-/*   Updated: 2026/01/20 05:12:15 by jaicastr         ###   ########.fr       */
+/*   Updated: 2026/01/20 05:13:20 by jaicastr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,19 +19,20 @@ __attribute__((__nonnull__(1)))
 t_file	ft_read_file(const char *restrict const fname)
 {
 	int					ret;
+	int					fd;
 	t_u8				stack[BUFSIZE];
 	t_vec				buffer;
 
 	buffer = ft_vec_new(BUFSIZE, sizeof(t_u8));
 	if (buffer.data == NULL)
 		return ((t_file){0});
-	ret = open(fname, O_RDONLY);
-	if (ret == -1)
+	fd = open(fname, O_RDONLY);
+	if (fd == -1)
 		return (ft_vec_free(&buffer), (t_file){0});
-	ret = read(ret, stack, BUFSIZE);
+	ret = read(fd, stack, BUFSIZE);
 	while (ret > 0 && ft_vec_extend(&buffer,
 				stack, sizeof(t_u8), (size_t)ret))
-		ret = read(ret, stack, BUFSIZE);
+		ret = read(fd, stack, BUFSIZE);
 	return ((t_file){.content = (t_u8 *)buffer.data,
 		.size = (t_uptr)buffer.head - (t_uptr)buffer.content});
 }
