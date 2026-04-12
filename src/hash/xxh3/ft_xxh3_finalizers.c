c658a93806d649d1647d11708402d7947f58d47a
j <castro.jaime@proton.me>
2026-04-12 16:24:31 +0200

    fix: inline finalizers

--- src/hash/xxh3/ft_xxh3_finalizers.c
+++ src/hash/xxh3/ft_xxh3_finalizers.c
@@ -6,22 +6,22 @@
 /*   By: jaicastr <jaicastr@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/12 05:54:23 by jaicastr          #+#    #+#             */
-/*   Updated: 2026/04/12 07:25:32 by jaicastr         ###   ########.fr       */
+/*   Updated: 2026/04/12 16:17:14 by jaicastr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "private/ft_p_xxh3.h"
 
 __attribute__((pure, __always_inline__))
-static inline t_u64a	ft_xxh3_mix2accs(t_blk64w acc, const t_u8 *secret)
+inline t_u64a	ft_xxh3_mix2accs(t_blk64w acc, const t_u8 *secret)
 {
 	return (ft_xxh3_mul128_fold64(
 			acc[0] ^ *(t_blk64r)secret,
 			acc[1] ^ *(t_blk64r)(secret + 8)));
 }
 
-__attribute__((pure))
-t_u64a	ft_xxh3_merge_accs(t_blk64w acc, const t_u8 *secret, t_u64 start)
+__attribute__((pure, __always_inline__))
+inline t_u64a	ft_xxh3_merge_accs(t_blk64w acc, const t_u8 *secret, t_u64 start)
 {
 	t_u64	result;
 
