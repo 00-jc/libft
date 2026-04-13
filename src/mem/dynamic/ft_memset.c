020d5e2f2c81e145e7a2dbdcdf207c3170bbea17
j <castro.jaime@proton.me>
2026-04-13 02:27:24 +0200

    remove upper bound computation in mem* dispatch

--- src/mem/dynamic/ft_memset.c
+++ src/mem/dynamic/ft_memset.c
@@ -6,7 +6,7 @@
 /*   By: jaicastr <jaicastr@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/16 17:13:42 by jaicastr          #+#    #+#             */
-/*   Updated: 2026/04/12 19:11:56 by jaicastr         ###   ########.fr       */
+/*   Updated: 2026/04/13 02:26:02 by jaicastr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,16 +18,16 @@ __attribute__((__nonnull__(1)))
 void	ft_memset(void *restrict dest,
 	const t_u8 b, size_t n)
 {
-	if (n >= 64)
-		ft_memset_512(dest, b, n);
-	else if (n >= 32)
-		ft_memset_256(dest, b, n);
-	else if (n >= 16)
-		ft_memset_128(dest, b, n);
-	else if (n >= 8)
-		ft_memset_64(dest, b, n);
-	else
+	if (n < 8)
 		ft_memset_naive(dest, b, n);
+	else if (n < 16)
+		ft_memset_64(dest, b, n);
+	else if (n < 32)
+		ft_memset_128(dest, b, n);
+	else if (n < 64)
+		ft_memset_256(dest, b, n);
+	else
+		ft_memset_512(dest, b, n);
 }
 
 #elif defined(__AVX2__)
@@ -36,14 +36,14 @@ __attribute__((__nonnull__(1)))
 void	ft_memset(void *restrict dest,
 	const t_u8 b, size_t n)
 {
-	if (n >= 32)
-		ft_memset_256(dest, b, n);
-	else if (n >= 16)
-		ft_memset_128(dest, b, n);
-	else if (n >= 8)
-		ft_memset_64(dest, b, n);
-	else
+	if (n < 8)
 		ft_memset_naive(dest, b, n);
+	else if (n < 16)
+		ft_memset_64(dest, b, n);
+	else if (n < 32)
+		ft_memset_128(dest, b, n);
+	else
+		ft_memset_256(dest, b, n);
 }
 
 #else
@@ -52,12 +52,12 @@ __attribute__((__nonnull__(1)))
 void	ft_memset(void *restrict dest,
 	const t_u8 b, size_t n)
 {
-	if (n >= 16)
-		ft_memset_128(dest, b, n);
-	else if (n >= 8)
+	if (n < 8)
+		ft_memset_naive(dest, b, n);
+	else if (n < 16)
 		ft_memset_64(dest, b, n);
 	else
-		ft_memset_naive(dest, b, n);
+		ft_memset_128(dest, b, n);
 }
 
 #endif
