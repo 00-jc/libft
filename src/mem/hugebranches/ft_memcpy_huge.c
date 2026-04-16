3e83b12e6e96f96a532488c803fa2244b7d86a95
j <castro.jaime@proton.me>
2026-04-17 01:40:21 +0200

    optimization: optimize short memcpys

--- src/mem/hugebranches/ft_memcpy_huge.c
+++ src/mem/hugebranches/ft_memcpy_huge.c
@@ -6,40 +6,47 @@
 /*   By: jaicastr <jaicastr@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/13 00:05:58 by jaicastr          #+#    #+#             */
-/*   Updated: 2026/04/17 01:00:41 by jaicastr         ###   ########.fr       */
+/*   Updated: 2026/04/17 01:39:57 by jaicastr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "private/ft_p_mem.h"
 
 __attribute__((__nonnull__(1, 2), __always_inline__))
+inline void	ft_memcpy_hugetail(void *restrict dest,
+	const void	*restrict const src, size_t n)
+{
+	size_t	i;
+
+	if (__builtin_expect(n != 0, 1))
+	{
+		i = -(1 < n) & 2;
+		((t_blk512wa)dest)[0] = ((t_blk512r)src)[0];
+		((t_blk512wa)dest)[0 < n] = ((t_blk512r)src)[0 < n];
+		((t_blk512wa)dest)[i] = ((t_blk512r)src)[i];
+		*((t_blk512w)ft_overlap(dest, sizeof(t_vu512a), n)) =
+			*((t_blk512r)ft_overlap(src, sizeof(t_vu512a), n));
+	}
+}
+
+__attribute__((__nonnull__(1, 2), __always_inline__))
 inline void	ft__cascade_cpy(void *restrict dest,
 	const void	*restrict const src, size_t i)
 {
-	t_vu512a	x[8];
+	t_vu512a	x[4];
 
-	ft_prefetchnta(src, sizeof(t_vu512) << 1);
-	ft_prefetchnta(src, sizeof(t_vu512) << 2);
-	ft_prefetchnta(src, sizeof(t_vu512) << 3);
-	ft_prefetchnta(dest, sizeof(t_vu512) << 1);
-	ft_prefetchnta(dest, sizeof(t_vu512) << 2);
-	ft_prefetchnta(dest, sizeof(t_vu512) << 3);
+	ft_prefetchnta(src, 0);
+	ft_prefetchnta(dest, 0);
+	ft_prefetchnta(src, sizeof(t_vu512));
+	ft_prefetchnta(dest, sizeof(t_vu512));
 	x[0] = ((t_blk512r)src)[i + 0];
 	x[1] = ((t_blk512r)src)[i + 1];
 	x[2] = ((t_blk512r)src)[i + 2];
 	x[3] = ((t_blk512r)src)[i + 3];
-	x[4] = ((t_blk512r)src)[i + 4];
-	x[5] = ((t_blk512r)src)[i + 5];
-	x[6] = ((t_blk512r)src)[i + 6];
-	x[7] = ((t_blk512r)src)[i + 7];
 	((t_blk512wa)dest)[i + 0] = x[0];
 	((t_blk512wa)dest)[i + 1] = x[1];
 	((t_blk512wa)dest)[i + 2] = x[2];
 	((t_blk512wa)dest)[i + 3] = x[3];
-	((t_blk512wa)dest)[i + 4] = x[4];
-	((t_blk512wa)dest)[i + 5] = x[5];
-	((t_blk512wa)dest)[i + 6] = x[6];
-	((t_blk512wa)dest)[i + 7] = x[7];
 }
 
 __attribute__((__nonnull__(1, 2), __always_inline__))
@@ -51,23 +58,18 @@ inline void	ft_memcpy_512_huge(void *restrict dest,
 	t_u8			*d;
 	const t_u8		*sr;
 
-	*(t_blk512w)dest = *(t_blk512r)src;
 	delta = (-(t_uptr)dest) & 63;
+	*(t_blk512w)dest = *(t_blk512r)src;
 	d = (t_u8 *)dest + delta;
 	sr = (const t_u8 *)src + delta;
 	n -= delta;
-	s.blks = n >> 6;
+	s.blks = (n - delta) >> 6;
 	s.i = 0;
-	while (s.i + 8 <= s.blks)
+	while (s.i + 4 < s.blks)
 	{
 		ft__cascade_cpy(d, sr, s.i);
-		s.i += 8;
-	}
-	while (s.i <= s.blks)
-	{
-		((t_blk512wa)dest)[s.i] = ((t_blk512r)src)[s.i];
-		++s.i;
+		s.i += 4;
 	}
 	s.i <<= 6;
-	ft_memcpy_256(d + s.i, sr + s.i, n - s.i);
+	ft_memcpy_hugetail(d + s.i, sr + s.i, n - s.i);
 }
