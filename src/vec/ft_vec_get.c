a55c3f1669c600d894ae699a5e9a95728106ff22
j <castro.jaime@proton.me>
2026-04-16 15:11:15 +0200

    fix: gcc analyzer complaints

--- src/vec/ft_vec_get.c
+++ src/vec/ft_vec_get.c
@@ -6,7 +6,7 @@
 /*   By: jaicastr <jaicastr@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/15 15:14:07 by jaicastr          #+#    #+#             */
-/*   Updated: 2026/04/13 19:07:51 by jaicastr         ###   ########.fr       */
+/*   Updated: 2026/04/16 15:10:12 by jaicastr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,12 +33,12 @@ inline const void	*ft_vec_peek_last(const t_vec *restrict const vec,
 	size_t type_size)
 {
 	return ((const void *)(-((t_uptr)vec->size != 0)
-		& (t_uptr)vec->data + ((vec->size - 1) * type_size)));
+		& ((t_uptr)vec->data + ((vec->size - 1) * type_size))));
 }
 
 __attribute__((__nonnull__(1), __always_inline__, pure))
 inline void	*ft_vec_get_last(const t_vec *restrict const vec, size_t type_size)
 {
 	return ((t_u8 *)(-((t_uptr)vec->size != 0)
-		& (t_uptr)vec->data + ((vec->size - 1) * type_size)));
+		& ((t_uptr)vec->data + ((vec->size - 1) * type_size))));
 }
