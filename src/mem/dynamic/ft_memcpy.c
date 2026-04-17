9ef45584fbc6977be5097f1bface11a849d5972f
j <castro.jaime@proton.me>
2026-04-17 02:26:12 +0200

    optimization: optimize 1-8 bytes memcpys

--- src/mem/dynamic/ft_memcpy.c
+++ src/mem/dynamic/ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: jaicastr <jaicastr@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/16 22:55:19 by jaicastr          #+#    #+#             */
-/*   Updated: 2026/04/17 02:01:01 by jaicastr         ###   ########.fr       */
+/*   Updated: 2026/04/17 02:25:35 by jaicastr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,9 @@ __attribute__((__nonnull__(1, 2)))
 void	ft_memcpy(void *restrict dest,
 	const void	*restrict const src, size_t n)
 {
-	if (n < 8)
+	if (__builtin_expect(n == 0, 0))
+		return ;
+	else if (n < 8)
 		ft_memcpy_naive(dest, src, n);
 	else if (n < 16)
 		ft_memcpy_64(dest, src, n);
