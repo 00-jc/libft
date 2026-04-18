ba460a02cd288dd3e0bb1970798569ba1d1e09c9
j <castro.jaime@proton.me>
2026-04-18 20:57:14 +0200

    fix: stop marking counters unused

--- src/perf/ft_perf_counter_interface.c
+++ src/perf/ft_perf_counter_interface.c
@@ -6,20 +6,20 @@
 /*   By: jaicastr <jaicastr@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/17 17:38:05 by jaicastr          #+#    #+#             */
-/*   Updated: 2026/04/18 20:41:30 by jaicastr         ###   ########.fr       */
+/*   Updated: 2026/04/18 20:57:02 by jaicastr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "private/ft_p_perf.h"
 
 __attribute__((__nonnull__(1), __always_inline__))
-inline void	ft_perf_counters_start(t_perf_counters __attribute__((unused)) c)
+inline void	ft_perf_counters_start(t_perf_counters c)
 {
 	ioctl((int)c[0], PERF_EVENT_IOC_ENABLE, 0);
 }
 
 __attribute__((__nonnull__(1), __always_inline__))
-inline void	ft_perf_counters_stop(t_perf_counters __attribute__((unused)) c)
+inline void	ft_perf_counters_stop(t_perf_counters c)
 {
 	ioctl((int)c[0], PERF_EVENT_IOC_DISABLE, 0);
 }
