c5693d7be734c161f081d69dbeac5acb62c54fc5
j <castro.jaime@proton.me>
2026-04-18 21:01:40 +0200

    fix: use group ioctl

--- src/perf/ft_perf_counter_interface.c
+++ src/perf/ft_perf_counter_interface.c
@@ -6,7 +6,7 @@
 /*   By: jaicastr <jaicastr@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/17 17:38:05 by jaicastr          #+#    #+#             */
-/*   Updated: 2026/04/18 20:57:02 by jaicastr         ###   ########.fr       */
+/*   Updated: 2026/04/18 21:01:31 by jaicastr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,17 +15,17 @@
 __attribute__((__nonnull__(1), __always_inline__))
 inline void	ft_perf_counters_start(t_perf_counters c)
 {
-	ioctl((int)c[0], PERF_EVENT_IOC_ENABLE, 0);
+	ioctl((int)c[0], PERF_EVENT_IOC_ENABLE, PERF_IOC_FLAG_GROUP);
 }
 
 __attribute__((__nonnull__(1), __always_inline__))
 inline void	ft_perf_counters_stop(t_perf_counters c)
 {
-	ioctl((int)c[0], PERF_EVENT_IOC_DISABLE, 0);
+	ioctl((int)c[0], PERF_EVENT_IOC_DISABLE, PERF_IOC_FLAG_GROUP);
 }
 
 __attribute__((__nonnull__(1), __always_inline__))
 inline void	ft_perf_counters_reset(t_perf_counters c)
 {
-	ioctl((int)c[0], PERF_EVENT_IOC_RESET, 0);
+	ioctl((int)c[0], PERF_EVENT_IOC_RESET, PERF_IOC_FLAG_GROUP);
 }
